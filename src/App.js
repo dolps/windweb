f3c70aa4f3e21a6ecbc29d4d1d608538ebd5a71a
dolps <thodol@netcompany.com>
2023-07-28 22:44:15 +0200

    added spots

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import SpotList from './component/SpotList';
 
 function App() {
   return (
@@ -7,17 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Spots
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SpotList/>
       </header>
+
     </div>
   );
 }
---
+++ src/component/SpotList.js
@@ -0,0 +1,69 @@
+const { useState, useEffect } = require("react")
+
+const SpotList = () => {
+    const [coinName, setCoinName] = useState(null);
+
+    const useSpotFetcher = () => {
+        const [spots, setSpotList] = useState(null);
+        const [fetched, setFetched] = useState(false);
+        const [loading, setLoading] = useState(false);
+
+        useEffect(() => {
+            console.log('hello')
+            const windApiUrl = "https://vindapi.herokuapp.com/api/spots";
+            setLoading(true);
+
+            fetch(windApiUrl, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'token': 'secret',
+                    'blabla': 'blabla'
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        setFetched(false);
+                        return null;
+                    }
+                    return response.json()
+                })
+                .then(json => {
+                    setLoading(false);
+                    setFetched(true);
+                    setSpotList(json);
+                })
+
+            console.log('spots')
+        }, [coinName])
+        return [spots, loading, fetched];
+    }
+
+    const [spots, loading, fetched] = useSpotFetcher();
+    const mapSpotData = () => {
+        if (!fetched) return <div>No data fetched</div>;
+        if (loading) return <div>Loading...</div>;
+        if (!spots) {
+            return <div>No Spot Data</div>;
+        } else {
+            return (
+                <div>
+                    <h1>Spots</h1>
+                    <div>{JSON.stringify(spots)}</div>
+
+                    <ul>
+                        {spots.map(d => (<li key={d.spotName}>{d.spotName}</li>))}
+                    </ul>
+
+                </div>
+            );
+        }
+    };
+
+    return (
+        <div>
+            {mapSpotData()}
+        </div>
+    );
+}
+export default SpotList;
