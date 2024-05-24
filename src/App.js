0b70b06f7cff8471cfe87fa1be1227b9f78d0188
thomas dolplads <dolplads@thomass-Laptop.local>
2024-05-24 15:01:47 +0200

    test deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" width={250} height={250}  />
           <p>
-            Windweb
+            Windwebb
           </p>
           <SpotList />
         </header>
